91af422249d44d2d69c493067205a56ee8496528
马海波 <1306364@qq.com>
2019-08-01 17:27:48 +0800

    try

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    哈哈
+    <el-switch></el-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+
+</style>
