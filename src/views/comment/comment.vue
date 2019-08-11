b4ed459d37d546ac1915682a75178013a0ed41f4
马海波 <1306364@qq.com>
2019-08-11 19:42:55 +0800

    评轮页面完成

--- src/views/comment/comment.vue
+++ src/views/comment/comment.vue
@@ -8,14 +8,26 @@
               <el-table-column label="标题" prop="title"></el-table-column>
               <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
               <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
-              <el-table-column label="状态"></el-table-column>
-              <el-table-column label="操作" width=120px></el-table-column>
+              <el-table-column label="状态">
+                  <template slot-scope='scope'>
+                      {{scope.row.comment_status?'正常':'关闭'}}
+                  </template>
+              </el-table-column>
+              <el-table-column label="操作" width="160px">
+                  <template slot-scope="scope">
+                      <el-button v-if="!scope.row.comment_status" type="success" size="small" @click="CommentStatus(scope.row)">打开评论</el-button>
+                      <el-button v-else type="danger" size="small" @click="CommentStatus(scope.row)">关闭评论</el-button>
+                  </template>
+              </el-table-column>
         </el-table>
         <el-pagination
             background
             layout="prev, pager, next"
-            :total="1000"
+            :total="total"
             style="margin-top:20px"
+            :page-size='allDate.per_page'
+            :current-page='allDate.page'
+            @current-change='changePage'
             >
         </el-pagination>
         </el-card>
@@ -27,19 +39,31 @@ export default {
   data () {
     return {
       allDate: {
-        total_comment_count: 0,
-        fans_comment_count: 0
+        response_type: 'comment',
+        page: 1,
+        per_page: 20
       },
-      comments: []
+      comments: [],
+      total: 0
     }
   },
   created () {
     this.getComments()
   },
   methods: {
+    async  CommentStatus (row) {
+      const { data: { data } } = await this.$http.put(`comments/status?article_id=${row.id}`, { allow_comment: !row.comment_status })
+      this.$message.success(data.allow_comment ? '打开评论成功' : '关闭评论成功')
+      row.comment_status = data.allow_comment
+    },
+    changePage (newPage) {
+      this.allDate.page = newPage
+      this.getComments()
+    },
     async getComments () {
       const { data: { data } } = await this.$http.get('articles', { params: this.allDate })
       this.comments = data.results
+      this.total = data.total_count
     }
   }
 }
